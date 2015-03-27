974b358bfdbc05aa47fbebccb5b1cd3746defe60
David Ernst <git@dsernst.com>
2015-03-27 03:35:07 -0700

    add ReactTable example

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var ReactTable = require('react-table');
 
 // If you are going to be using stores, be sure to first load in the `Fluxxor`
 // module.
@@ -39,6 +40,7 @@ module.exports = React.createClass({
     return (
       <div className='home-page'>
         <Header />
+        <Highscores />
       </div>
     );
   }
@@ -68,6 +70,21 @@ var Tagline = React.createClass({
   render: function () {
     return (
       <h2>A Numbers Game</h2>
+    );
+  }
+});
+
+var data = [
+  {favoriteColor:'blue',  age: 30, name: "Athos",      job: "Musketeer"},
+  {favoriteColor: 'red' ,  age: 33, name: "Porthos",    job: "Musketeer"},
+  {favoriteColor: 'blue' ,  age: 27, name: "Aramis",     job: "Musketeer"},
+  {favoriteColor: 'orange' ,  age: 25, name: "d'Artagnan", job: "Guard"}
+];
+
+var Highscores = React.createClass({
+  render: function () {
+    return (
+      <ReactTable data={data} />
     );
   }
 });
